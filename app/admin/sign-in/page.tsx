b4129829572169e2f691cbc0f0d5cd6c7fc2f9f8
iamchitta07 <nathchittajit8@gmail.com>
2026-05-29 22:30:34 +0530

    Added: Sign-in page

---
+++ app/admin/sign-in/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { InputDemo } from "../_components/InputField";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="text-white min-h-screen md:min-h-0 md:mt-60 lg:mt-20 w-full flex flex-col justify-center items-center gap-2 md:gap-3">
+      <InputDemo type="text" placeHolder="Enter your name" title="Name" id="name" />
+      <InputDemo
+        type="email"
+        placeHolder="Enter your mail (institute mail preferred)"
+        title="E-mail"
+        id="mail"
+      />
+      <InputDemo type="password" placeHolder="Secret Key" title="Secret Key" id="secret_key" />
+      <div className="w-[60%] flex justify-center gap-5 md:gap-10 my-5">
+        <Button className="md:text-lg md:px-5 md:py-4 md:rounded-md lg:text-xl lg:px-8 lg:py-6">
+          New Here ?
+        </Button>
+        <Button className="md:text-lg md:px-5 md:py-4 md:rounded-md lg:text-xl lg:px-8 lg:py-6">
+          Sign-In
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
