67f699ae187753bc1d2870017209b3f693b28907
iamchitta07 <nathchittajit8@gmail.com>
2026-06-20 11:16:46 +0530

    Added: new-project page

--- app/admin/new-project/page.tsx
+++ app/admin/new-project/page.tsx
@@ -3,15 +3,13 @@
 import { useRouter } from "next/navigation";
 import { ChevronLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import ImageUploader from "../_components/ImageUploader";
 import { InputDemo } from "../_components/InputField";
-import { DateTimePicker } from "../_components/DateTimemPicker";
 import { ContentBox } from "../_components/ContentBox";
 
 const Home = () => {
   const router = useRouter();
   return (
-    <div className="w-full mt-12 lg:mt-16 px-5 lg:px-20">
+    <div className="w-full mt-12 lg:mt-16 px-5 lg:px-20 flex-1">
       <div className="hidden md:block">
         <Button
           className="flex gap-0 py-0.5 px-3 rounded-full bg-white text-black"
@@ -21,32 +19,23 @@ const Home = () => {
           Back
         </Button>
       </div>
-      <div className="w-full flex flex-col items-center gap-3 mb-10 mt-10">
-        <ImageUploader />
+      <div className="w-full flex flex-col items-center gap-3 mb-10 mt-20 lg:mt-10">
         <InputDemo
           id="title"
           title="Title"
           type="text"
           className="w-75 md:w-sm"
-          placeHolder="Enter title of the event"
+          placeHolder="Enter title of the project"
         />
         <InputDemo
-          id="venue"
-          title="Venue"
+          id="project-link"
+          title="Project Link"
           type="text"
           className="w-75 md:w-sm"
-          placeHolder="Enter venue of the event"
+          placeHolder="Enter project link"
         />
-        <DateTimePicker />
-        <InputDemo
-          id="reg-link"
-          title="Regitration Link"
-          type="text"
-          className="w-75 md:w-sm"
-          placeHolder="Enter registration"
-        />
-        <ContentBox placeHolder="Enter content of the event" />
-        <Button className="px-10 py-5 md:py-6 text-lg md:text-xl">Save Event</Button>
+        <ContentBox placeHolder="Enter details of the project" />
+        <Button className="px-10 py-5 md:py-6 text-lg md:text-xl">Save Project</Button>
       </div>
     </div>
   );
